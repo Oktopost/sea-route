74fd145e5642aea2ebaad8c5e4e89e0d6bfe40b0
Alexey Puchkov <alexey@oktopost.com>
2017-05-07 13:55:35 +0300

    Fix ConstParamDecorator

--- src/params/ConstParamDecorator.js
+++ src/params/ConstParamDecorator.js
@@ -12,36 +12,24 @@ require('../../namespace').namespace('SeaRoute.params', function(root) {
 	 * @class SeaRoute.params.ConstParamDecorator
 	 * 
 	 * @param {string} name
-	 * @param {SeaRoute.params} param
+	 * @param {SeaRoute.params.Param} param
 	 * 
 	 * @property {string} _name
 	 * @property {SeaRoute.params.Param} _original
 	 */
 	var ConstParamDecorator = function(name, param) {
 		var self = this;
+		Param.call(this, name);
 		
 		
-		this._name = name;
-		
-		
-		obj.forEach.pair(this, function (key, val) {
-			if (key !== 'name' && is.function(val)) {
-				self[key] = val.bind(param);
-			}
-		});
+		this.validate	= param.validate;
+		this.extract	= param.extract;
+		this.encode		= param.encode;
 	};
 	
 	
 	ConstParamDecorator.prototype = Object.create(Param.prototype);
 	ConstParamDecorator.prototype.constructor = ConstParamDecorator;
-	
-
-	/**
-	 * @return {string}
-	 */
-	ConstParamDecorator.prototype.name = function () {
-		return this._name;
-	};
 	
 	
 	this.ConstParamDecorator = ConstParamDecorator;
