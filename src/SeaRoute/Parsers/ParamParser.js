eb3c8bc3bceb3dda36497c53db577bf833a27cb8
Alexey Puchkov <alexey.pkv@gmail.com>
2017-07-20 09:16:00 +0300

    Add unit test for NumberParam in ParamParser

--- src/SeaRoute/Parsers/ParamParser.js
+++ src/SeaRoute/Parsers/ParamParser.js
@@ -1,8 +1,8 @@
-namespace('SeaRoute.Parsers', function(root) {
-	'use strict';
-	
-	var Param			= root.SeaRoute.ParamType.Param;	
+namespace('SeaRoute.Parsers', function(root)
+{
+	var Param			= root.SeaRoute.ParamType.Param;
 	var IntParam		= root.SeaRoute.ParamType.IntParam;
+	var NumberParam		= root.SeaRoute.ParamType.NumberParam;
 	var OneOfParam		= root.SeaRoute.ParamType.OneOfParam;
 	var RegexParam		= root.SeaRoute.ParamType.RegexParam;
 	var JsonParam		= root.SeaRoute.ParamType.JsonParam;
@@ -20,7 +20,8 @@ namespace('SeaRoute.Parsers', function(root) {
 	 * @name SeaRoute.Parsers.ParamParser
 	 */
 	var ParamParser = {
-		FLAGS: {
+		FLAGS:
+		{
 			DEFAULT_VALUE:	'def',
 			AUTO_FILL:		'fill',
 			OPTIONAL:		'optional',
@@ -28,7 +29,8 @@ namespace('SeaRoute.Parsers', function(root) {
 			TYPE:			'type'
 		},
 		
-		TYPES: {
+		TYPES:
+		{
 			INT:		'int',
 			ARRAY:		'array',
 			REGEX:		'regex',
@@ -36,9 +38,10 @@ namespace('SeaRoute.Parsers', function(root) {
 			WILDCARD:	'wildcard'
 		},
 		
-		CREATORS: {
-			'num': 			'_createIntParam',
-			'number':		'_createIntParam',
+		CREATORS:
+		{
+			'num': 			'_createNumberParam',
+			'number':		'_createNumberParam',
 			
 			'int': 			'_createIntParam',
 			'array': 		'_createOneOfParam',
@@ -50,43 +53,60 @@ namespace('SeaRoute.Parsers', function(root) {
 		},
 		
 		
-		_createIntParam: function (name, config) {
-			var param = new IntParam(name);
+		_setMinMax: function (param, config)
+		{
+			if (is.defined(config.min) && is.number(config.min))
+				param.setMin(config.min);
 			
-			if (is.defined(config.min) && is.number(config.min)) {
-				param.setMin(config.min)
-			}
-			
-			if (is.defined(config.max) && is.number(config.max)) {
-				param.setMax(config.max)
-			}
+			if (is.defined(config.max) && is.number(config.max))
+				param.setMax(config.max);
 			
 			return param;
 		},
 		
-		_createOneOfParam: function (name, config) {
-			if (!is.array(config.values)) {
-				throw 'The config "values" is required for param of type "array" and must be an Array of strings'; 
+		
+		_createNumberParam: function (name, config)
+		{
+			var param = new NumberParam(name);
+			return ParamParser._setMinMax(param, config);
+		},
+		
+		_createIntParam: function (name, config)
+		{
+			var param = new IntParam(name);
+			return ParamParser._setMinMax(param, config);
+		},
+		
+		_createOneOfParam: function (name, config)
+		{
+			if (!is.array(config.values))
+			{
+				throw new Error('The config "values" is required for param of type "array" and must be an Array of strings'); 
 			}
 			
 			return new OneOfParam(name, ['a']);
 		},
 		
-		_createRegexParam: function (name, config) {
-			if (is.undefined(config.regex) || !(config.regex instanceof RegExp)) {
-				throw 'The config "regex" is required for param of type "regex" and must be a RegExp object'; 
+		_createRegexParam: function (name, config)
+		{
+			if (is.undefined(config.regex) || !(config.regex instanceof RegExp))
+			{
+				throw new Error('The config "regex" is required for param of type "regex" and must be a RegExp object'); 
 			}
 			
 			return new RegexParam(name, config.regex);
 		},
 		
-		_createJsonParam: function (name, config) {
+		_createJsonParam: function (name)
+		{
 			return new JsonParam(name);
 		},
 		
-		_createWildcardParam: function (name, config) {
-			if (!is.string(config.exp)) {
-				throw 'The config "exp" is required for param of type "wildcard" and must be a string'; 
+		_createWildcardParam: function (name, config)
+		{
+			if (!is.string(config.exp))
+			{
+				throw new Error('The config "exp" is required for param of type "wildcard" and must be a string'); 
 			}
 			
 			return new WildcardParam(name, config.exp);
@@ -99,7 +119,8 @@ namespace('SeaRoute.Parsers', function(root) {
 		 * @return {SeaRoute.ParamType.Param}
 		 * @private
 		 */
-		_createByType: function (name, config) {
+		_createByType: function (name, config)
+		{
 			if (!is.defined(config[this.FLAGS.TYPE]))
 			{
 				return new Param(name);
@@ -107,7 +128,8 @@ namespace('SeaRoute.Parsers', function(root) {
 			
 			var type = config[this.FLAGS.TYPE];
 			
-			if (is.undefined(this.CREATORS[type])) {
+			if (is.undefined(this.CREATORS[type]))
+			{
 				throw 'Invalid type ' + type;
 			}
 			
@@ -120,20 +142,34 @@ namespace('SeaRoute.Parsers', function(root) {
 		 * @return {SeaRoute.ParamType.Param}
 		 * @private
 		 */
-		_createParam: function (name, config) {
-			if (is.array(config)) {
+		_createParam: function (name, config)
+		{
+			if (is.array(config))
+			{
 				return new OneOfParam(name, config);
-			} else if (is.string(config)) {
+			}
+			else if (is.string(config))
+			{
 				return new RegexParam(name, new RegExp(config.replace('*', '.*')));
-			} else if (config instanceof Param) {
+			}
+			else if (config instanceof Param)
+			{
 				return new PredefinedParamDecorator(name, config);
-			} else if (config instanceof RegExp) {
+			}
+			else if (config instanceof RegExp)
+			{
 				return new RegexParam(name, config);
-			} else if (!is.object(config)) {
+			}
+			else if (!is.object(config))
+			{
 				throw new Error('Unexpected route config for ' + name + ', got ' + JSON.stringify(config));
-			} else if (is.defined(config[this.FLAGS.VALUE])) {
+			}
+			else if (is.defined(config[this.FLAGS.VALUE]))
+			{
 				return this._createParam(name, config[this.FLAGS.VALUE]);
-			} else {
+			}
+			else
+			{
 				return this._createByType(name, config);
 			}
 		},
@@ -146,18 +182,22 @@ namespace('SeaRoute.Parsers', function(root) {
 		_setGenericConfig: function (prm, config) {
 			
 			// Set default value
-			if (is.defined(config[this.FLAGS.DEFAULT_VALUE])) {
+			if (is.defined(config[this.FLAGS.DEFAULT_VALUE]))
+			{
 				prm.setDefaultValue(config[this.FLAGS.DEFAULT_VALUE]);
 			}
 			
 			// Set optional flag.
-			if (is.defined(config[this.FLAGS.OPTIONAL])) {
+			if (is.defined(config[this.FLAGS.OPTIONAL]))
+			{
 				prm.setIsOptional(is(config[this.FLAGS.OPTIONAL]));
 			}
 			
 			// Set auto fill flag.
-			if (is.defined(config[this.FLAGS.AUTO_FILL])) {
-				if (!prm.hasDefaultValue()) {
+			if (is.defined(config[this.FLAGS.AUTO_FILL]))
+			{
+				if (!prm.hasDefaultValue())
+				{
 					throw new Error(
 						'Default value must be provided for a url with the "' + this.FLAGS.AUTO_FILL + '" flag');
 				}
@@ -172,10 +212,12 @@ namespace('SeaRoute.Parsers', function(root) {
 		 * @param {{}|[]|string|RegExp|SeaRoute.ParamType.Param} object
 		 * @return {SeaRoute.ParamType.Param}
 		 */
-		parse: function (name, object) {
+		parse: function (name, object)
+		{
 			var param = this._createParam(name, object);
 			
-			if (is.object(object) && !(object instanceof Param)) {
+			if (is.object(object) && !(object instanceof Param))
+			{
 				this._setGenericConfig(param, object);
 			}
 			
--- test/src/SeaRoute/Parsers/ParamParser.js
+++ test/src/SeaRoute/Parsers/ParamParser.js
@@ -1,11 +1,9 @@
-'use strict';
-
-
 const SeaRoute = require('../../../index');
 
 
 const Param						= SeaRoute.ParamType.Param;
 const IntParam					= SeaRoute.ParamType.IntParam;
+const NumberParam				= SeaRoute.ParamType.NumberParam;
 const JsonParam					= SeaRoute.ParamType.JsonParam;
 const RegexParam				= SeaRoute.ParamType.RegexParam;
 const OneOfParam				= SeaRoute.ParamType.OneOfParam;
@@ -17,13 +15,17 @@ const ParamParser = SeaRoute.Parsers.ParamParser;
 const assert = require('chai').assert;
 
 
-suite('ParamParser', () => {
-	suite('Default data', () => {
-		test('return generic param', () => {
+suite('ParamParser', () =>
+{
+	suite('Default data', () =>
+	{
+		test('return generic param', () =>
+		{
 			assert.instanceOf(ParamParser.parse('a', {}), Param);
 		});
 		
-		test('default flags', () => {
+		test('default flags', () =>
+		{
 			var obj = ParamParser.parse('a', {});
 			
 			assert.isFalse(obj.hasDefaultValue());
@@ -31,43 +33,53 @@ suite('ParamParser', () => {
 			assert.isFalse(obj.isOptional());
 		});
 		
-		test('name passed', () => {
+		test('name passed', () =>
+		{
 			var obj = ParamParser.parse('a', {});
 			assert.equal('a', obj.name());
 		});
 	});
 	
 	
-	suite('flags', () => {
-		suite('optional', () => {
-			test('true', () => {
+	suite('flags', () =>
+	{
+		suite('optional', () =>
+		{
+			test('true', () =>
+			{
 				var obj = ParamParser.parse('a', { optional: true });
 				assert.isTrue(obj.isOptional());
 			});
 			
-			test('false', () => {
+			test('false', () =>
+			{
 				var obj = ParamParser.parse('a', { optional: false });
 				assert.isFalse(obj.isOptional());
 			});
 		});
 		
-		suite('auto fill', () => {
-			test('true', () => {
+		suite('auto fill', () =>
+		{
+			test('true', () =>
+			{
 				var obj = ParamParser.parse('a', { def: 1, fill: true });
 				assert.isTrue(obj.isAutoFillURL());
 			});
 			
-			test('false', () => {
+			test('false', () =>
+			{
 				var obj = ParamParser.parse('a', { def: 1, fill: false });
 				assert.isFalse(obj.isAutoFillURL());
 			});
 			
-			test('no default value, error thrown', () => {
+			test('no default value, error thrown', () =>
+			{
 				assert.throws(() => { ParamParser.parse('a', { fill: false }) });
 			});
 		});
 		
-		test('default value used', () => {
+		test('default value used', () =>
+		{
 			var obj = ParamParser.parse('a', { def: 123 });
 			assert.isTrue(obj.hasDefaultValue());
 			assert.equal(123, obj.defaultValue());
@@ -75,8 +87,10 @@ suite('ParamParser', () => {
 	});
 	
 	
-	suite('non config object', () => {
-		test('array', () => {
+	suite('non config object', () =>
+	{
+		test('array', () =>
+		{
 			var obj = ParamParser.parse('a', ['a', 'b']);
 			
 			assert.instanceOf(obj, OneOfParam);
@@ -84,7 +98,8 @@ suite('ParamParser', () => {
 			assert.isFalse(obj.validate('c'));
 		});
 		
-		test('RegExp', () => {
+		test('RegExp', () =>
+		{
 			var obj = ParamParser.parse('a', /a/i);
 			
 			assert.instanceOf(obj, RegexParam);
@@ -93,7 +108,8 @@ suite('ParamParser', () => {
 			assert.isFalse(obj.validate('b'));
 		});
 		
-		test('string', () => {
+		test('string', () =>
+		{
 			var obj = ParamParser.parse('a', 'a*b');
 			
 			assert.instanceOf(obj, RegexParam);
@@ -103,7 +119,8 @@ suite('ParamParser', () => {
 			assert.isFalse(obj.validate('ac'));
 		});
 		
-		test('Param', () => {
+		test('Param', () =>
+		{
 			var prm = new Param('n');
 			var obj = ParamParser.parse('a', prm);
 			
@@ -111,51 +128,80 @@ suite('ParamParser', () => {
 			assert.equal('a', obj.name());
 		});
 		
-		test('invalid', () => {
+		test('invalid', () => 
+		{
 			assert.throws(() => { ParamParser.parse('a', true) });
 		});
 		
-		test('value flag used', () => {
+		test('value flag used', () =>
+		{
 			var obj = ParamParser.parse('a', { value: ['a', 'b'] });
 			assert.instanceOf(obj, OneOfParam);
 		});
 	});
 	
-	suite('create by type', () => {
-		test('int', () => {
+	suite('create by type', () =>
+	{
+		test('int', () =>
+		{
 			var obj = ParamParser.parse('a', { type: 'int' });
 			assert.instanceOf(obj, IntParam);
 		});
 		
-		test('array', () => {
+		test('number', () =>
+		{
+			var obj = ParamParser.parse('a', { type: 'number' });
+			assert.instanceOf(obj, NumberParam);
+		});
+		
+		test('number as num', () =>
+		{
+			var obj = ParamParser.parse('a', { type: 'num' });
+			assert.instanceOf(obj, NumberParam);
+		});
+		
+		test('array', () =>
+		{
 			var obj = ParamParser.parse('a', { type: 'array', values: [] });
 			assert.instanceOf(obj, OneOfParam);
 		});
 		
-		test('regex', () => {
+		test('regex', () =>
+		{
 			var obj = ParamParser.parse('a', { type: 'regex', regex: /a/ });
 			assert.instanceOf(obj, RegexParam);
 		});
 		
-		test('wildcard', () => {
+		test('wildcard', () =>
+		{
 			var obj = ParamParser.parse('a', { type: 'wildcard', exp: 'a*b' });
 			assert.instanceOf(obj, WildcardParam);
 		});
 		
-		test('invalid', () => {
+		test('wildcard as wild', () =>
+		{
+			var obj = ParamParser.parse('a', { type: 'wild', exp: 'a*b' });
+			assert.instanceOf(obj, WildcardParam);
+		});
+		
+		test('invalid', () =>
+		{
 			assert.throws(() => { ParamParser.parse('a', { type: 'INVALID' }) });
 		});
 	});
 	
-	suite('Int param', () => {
-		test('Default range', () => {
+	suite('Int param', () =>
+	{
+		test('Default range', () =>
+		{
 			var obj = ParamParser.parse('a', { type: 'int' });
 			
 			assert.isTrue(obj.validate(Number.MIN_VALUE));
 			assert.isTrue(obj.validate(Number.MAX_VALUE));
 		});
 		
-		test('Range set', () => {
+		test('Range set', () =>
+		{
 			var obj = ParamParser.parse('a', { type: 'int', min: 2, max: 10 });
 			
 			assert.isTrue(obj.validate(2));
@@ -165,16 +211,41 @@ suite('ParamParser', () => {
 		});
 	});
 	
-	suite('One of param', () => {
-		test('No data set, throw error', () => {
+	suite('Number param', () =>
+	{
+		test('Default range', () =>
+		{
+			var obj = ParamParser.parse('a', { type: 'number' });
+			
+			assert.isTrue(obj.validate(Number.MIN_VALUE));
+			assert.isTrue(obj.validate(Number.MAX_VALUE));
+		});
+		
+		test('Range set', () =>
+		{
+			var obj = ParamParser.parse('a', { type: 'number', min: 2.2, max: 10 });
+			
+			assert.isTrue(obj.validate(2.3));
+			assert.isTrue(obj.validate(10));
+			assert.isFalse(obj.validate(2.1));
+			assert.isFalse(obj.validate(11));
+		});
+	});
+	
+	suite('One of param', () =>
+	{
+		test('No data set, throw error', () =>
+		{
 			assert.throws(() => { ParamParser.parse('a', { type: 'array' }); });
 		});
 		
-		test('Data not array, throw error', () => {
+		test('Data not array, throw error', () =>
+		{
 			assert.throws(() => { ParamParser.parse('a', { type: 'array', values: 2 }); });
 		});
 		
-		test('Range set', () => {
+		test('Range set', () =>
+		{
 			var obj = ParamParser.parse('a', { type: 'array', values: ['a', 'b'] });
 			
 			assert.isTrue(obj.validate('a'));
@@ -182,16 +253,20 @@ suite('ParamParser', () => {
 		});
 	});
 	
-	suite('Regex param', () => {
-		test('No data set, throw error', () => {
+	suite('Regex param', () =>
+	{
+		test('No data set, throw error', () =>
+		{
 			assert.throws(() => { ParamParser.parse('a', { type: 'regex' }); });
 		});
 		
-		test('Data not a regex, throw error', () => {
+		test('Data not a regex, throw error', () =>
+		{
 			assert.throws(() => { ParamParser.parse('a', { type: 'regex', regex: 2 }); });
 		});
 		
-		test('Valid regex', () => {
+		test('Valid regex', () =>
+		{
 			var obj = ParamParser.parse('a', { type: 'regex', regex: /a/ });
 			
 			assert.isTrue(obj.validate('a'));
@@ -199,23 +274,29 @@ suite('ParamParser', () => {
 		});
 	});
 	
-	suite('Json param', () => {
-		test('Json param created', () => {
+	suite('Json param', () =>
+	{
+		test('Json param created', () =>
+		{
 			var obj = ParamParser.parse('a', { type: 'json' });
 			assert.instanceOf(obj, JsonParam);
 		});
 	});
 	
-	suite('Wildcard param', () => {
-		test('No data set, throw error', () => {
+	suite('Wildcard param', () =>
+	{
+		test('No data set, throw error', () =>
+		{
 			assert.throws(() => { ParamParser.parse('a', { type: 'wildcard' }); });
 		});
 		
-		test('Data not a string, throw error', () => {
+		test('Data not a string, throw error', () =>
+		{
 			assert.throws(() => { ParamParser.parse('a', { type: 'wildcard', exp: 2 }); });
 		});
 		
-		test('Valid expression', () => {
+		test('Valid expression', () =>
+		{
 			var obj = ParamParser.parse('a', { type: 'wildcard', exp: 'a*b' });
 			
 			assert.isTrue(obj.validate('ab'));
